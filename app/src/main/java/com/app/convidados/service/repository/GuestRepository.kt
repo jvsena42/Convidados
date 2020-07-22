7a93c2bb49f96f6319f486670a61da5ff06573d8
jvsena42 <jvsena16@gmail.com>
2020-07-22 06:18:37 -0300

    singleton

--- app/src/main/java/com/app/convidados/service/repository/GuestRepository.kt
+++ app/src/main/java/com/app/convidados/service/repository/GuestRepository.kt
@@ -1,8 +1,27 @@
 package com.app.convidados.service.repository
 
+import android.content.Context
 import com.app.convidados.service.model.GuestModel
 
-class GuestRepository{
+class GuestRepository private constructor(context:Context){
+
+    /*criar um singleton
+    * 1 fechar o construtor
+    * 2 criar um objeto estático, porque uma classe dde construtor fechado não pode ser estática*/
+
+    private var mGuestDataBaseHelper: GuestDataBaseHelper = GuestDataBaseHelper(context)
+
+    companion object{
+
+        private lateinit var repository: GuestRepository
+
+        fun getInstance(context: Context) : GuestRepository{
+            if (!::repository.isInitialized){
+                repository = GuestRepository(context)
+            }
+            return repository
+        }
+    }
 
     //CRUD - Create, Read, Update, Delete
 
--- app/src/main/java/com/app/convidados/viewmodel/GuestsFormViewModel.kt
+++ app/src/main/java/com/app/convidados/viewmodel/GuestsFormViewModel.kt
@@ -1,15 +1,18 @@
 package com.app.convidados.viewmodel
 
+import android.app.Application
+import android.content.Context
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.app.convidados.service.model.GuestModel
 import com.app.convidados.service.repository.GuestRepository
 
-class GuestsFormViewModel: ViewModel() {
+class GuestsFormViewModel(application: Application): AndroidViewModel(application) {
 
-    private val mGuestRepository: GuestRepository =
-        GuestRepository()
+    private val mcontext = application.applicationContext
+    private val mGuestRepository: GuestRepository =GuestRepository.getInstance(mcontext)
 
     private var mSaveGuest = MutableLiveData<Boolean>()
     val saveGuest: LiveData<Boolean> = mSaveGuest
