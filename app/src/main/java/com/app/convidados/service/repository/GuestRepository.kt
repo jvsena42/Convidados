88666a012875feacf66ec987bc5aac753d9425bc
jvsena42 <jvsena16@gmail.com>
2020-07-23 05:22:10 -0300

    inserção no banco de dados

--- app/src/main/java/com/app/convidados/service/repository/GuestRepository.kt
+++ app/src/main/java/com/app/convidados/service/repository/GuestRepository.kt
@@ -1,9 +1,12 @@
 package com.app.convidados.service.repository
 
+import android.content.ContentValues
 import android.content.Context
+import com.app.convidados.service.constants.DataBaseConstants
 import com.app.convidados.service.model.GuestModel
+import java.lang.Exception
 
-class GuestRepository private constructor(context:Context){
+class GuestRepository private constructor(context: Context) {
 
     /*criar um singleton
     * 1 fechar o construtor
@@ -11,12 +14,12 @@ class GuestRepository private constructor(context:Context){
 
     private var mGuestDataBaseHelper: GuestDataBaseHelper = GuestDataBaseHelper(context)
 
-    companion object{
+    companion object {
 
         private lateinit var repository: GuestRepository
 
-        fun getInstance(context: Context) : GuestRepository{
-            if (!::repository.isInitialized){
+        fun getInstance(context: Context): GuestRepository {
+            if (!::repository.isInitialized) {
                 repository = GuestRepository(context)
             }
             return repository
@@ -25,30 +28,40 @@ class GuestRepository private constructor(context:Context){
 
     //CRUD - Create, Read, Update, Delete
 
-    fun getAll(): List<GuestModel>{
+    fun getAll(): List<GuestModel> {
         val list: MutableList<GuestModel> = ArrayList()
         return list
     }
 
-    fun getPresent(): List<GuestModel>{
+    fun getPresent(): List<GuestModel> {
         val list: MutableList<GuestModel> = ArrayList()
         return list
     }
 
-    fun getAbsent(): List<GuestModel>{
+    fun getAbsent(): List<GuestModel> {
         val list: MutableList<GuestModel> = ArrayList()
         return list
     }
 
-    fun save(guest: GuestModel){
+    fun save(guest: GuestModel):Boolean {
+        return try {
+            val db = mGuestDataBaseHelper.writableDatabase
+
+            val contentValues = ContentValues()
+            contentValues.put(DataBaseConstants.GUEST.COLUMNS.NAME, guest.name)
+            contentValues.put(DataBaseConstants.GUEST.COLUMNS.PRESENCE, guest.presence)
+            db.insert(DataBaseConstants.GUEST.TABLE_NAME, null, contentValues)
+            true
+        } catch (e: Exception) {
+            false
+        }
+    }
+
+    fun update(guest: GuestModel) {
 
     }
 
-    fun update(guest: GuestModel){
-
-    }
-
-    fun delete(guest: GuestModel){
+    fun delete(guest: GuestModel) {
 
     }
 
